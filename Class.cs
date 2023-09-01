270e604c92e12e2d6538e809da466f02c8d6ced1
malinaafemi <81665270+malinaafemi@users.noreply.github.com>
2023-09-01 14:24:35 +0700

    Add files

---
+++ Class.cs
@@ -0,0 +1,11 @@
+﻿namespace Hello
+{
+    public class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hellow World!");
+        }
+
+    }
+}
